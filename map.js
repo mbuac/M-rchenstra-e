c46c5d3f71b39620cc22ad3d56691411927b945e
mbuac <34254939+mbuac@users.noreply.github.com>
2018-12-30 04:02:36 +0100

    Website-Gestaltung
    Ideen für das Design

---
+++ map.js
@@ -0,0 +1,11 @@
+function karte(){
+    
+}
+
+//function over(){
+ //   document.getElementById("links").style.color="olive";
+ //}
+
+//function out(){
+ //   document.getElementById("links").style.color="black";
+ //}
